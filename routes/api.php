58b9eaa79f8b0fd7eda98082f810246a9f83ac3a
peijun333 <unix.043@gmail.com>
2019-05-24 02:49:36 +0900

    写真詳細ページの作成

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,5 @@ Route::post('/login', 'Auth\LoginController@login')->name('login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/photos', 'PhotoController@index')->name('photo.index');
+Route::get('/photos/{id}', 'PhotoController@show')->name('photo.show');
 Route::post('/photos', 'PhotoController@create')->name('photo.create');
